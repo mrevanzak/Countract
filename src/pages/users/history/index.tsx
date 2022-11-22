d3de38e0e87839a97d9f402999880b418541b716
mrevanzak <mrevanzak@gmail.com>
2022-11-22 16:55:40 +0700

    feat: table responsiveness

--- src/pages/users/history/index.tsx
+++ src/pages/users/history/index.tsx
@@ -1,4 +1,3 @@
-import { Table } from '@mantine/core';
 import toast, { Toaster } from 'react-hot-toast';
 import { useQuery } from 'react-query';
 
@@ -53,78 +52,112 @@ function HistoryPage() {
       <main>
         <section className='m-6'>
           <h3>Riwayat Akses Dokumen</h3>
-          <div className='mt-3 rounded-md bg-white p-5'>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Pengakses</th>
-                  <th>Dokumen</th>
-                  <th>Tanggal</th>
-                  <th>Detail</th>
-                </tr>
-              </thead>
-              <tbody>
-                {isLoading ? (
-                  <div>Loading...</div>
-                ) : isError ? (
-                  <div>{error.message}</div>
-                ) : (
-                  data &&
-                  data.map((item, itemIdx) => (
-                    <tr key={itemIdx}>
-                      <td>{item.nama_pengakses}</td>
-                      <td>{item.jenis_dokumen}</td>
-                      <td>{getISODatetoWIB(item.tanggal)} WIB</td>
-                      <td>
-                        {(() => {
-                          switch (item.status_diterima) {
-                            case 1:
-                              return (
-                                <p className='font-bold text-green-500'>
-                                  Diterima
-                                </p>
-                              );
-                            case -1:
-                              return (
-                                <p className='font-bold text-red-500'>
-                                  Ditolak
-                                </p>
-                              );
-                            case 0:
-                              return (
-                                <div className='flex space-x-2'>
-                                  <button
-                                    onClick={() =>
-                                      verifyAccess(
-                                        Status.DITERIMA,
-                                        item.id_riwayat
-                                      )
-                                    }
-                                    className='bg-green-500 px-4 py-2 text-white'
-                                  >
-                                    Terima
-                                  </button>
-                                  <button
-                                    onClick={() =>
-                                      verifyAccess(
-                                        Status.DITOLAK,
-                                        item.id_riwayat
-                                      )
-                                    }
-                                    className='bg-red-500 px-4 py-2 text-white'
-                                  >
-                                    Tolak
-                                  </button>
-                                </div>
-                              );
-                          }
-                        })()}
-                      </td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            </Table>
+          <div className='rounded-mdp-5 mt-3'>
+            <div className='mt-8 flex flex-col'>
+              <div className='-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+                <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
+                  <div className='overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg'>
+                    <table className='min-w-full divide-y divide-gray-300'>
+                      <thead className='bg-gray-50'>
+                        <tr>
+                          <th
+                            scope='col'
+                            className='py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6'
+                          >
+                            Pengakses
+                          </th>
+                          <th
+                            scope='col'
+                            className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
+                          >
+                            Dokumen
+                          </th>
+                          <th
+                            scope='col'
+                            className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
+                          >
+                            Tanggal
+                          </th>
+                          <th
+                            scope='col'
+                            className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
+                          >
+                            Detail
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className='divide-y divide-gray-200 bg-white'>
+                        {isLoading ? (
+                          <div>Loading...</div>
+                        ) : isError ? (
+                          <div>{error.message}</div>
+                        ) : (
+                          data &&
+                          data.map((item, itemIdx) => (
+                            <tr key={itemIdx}>
+                              <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6'>
+                                {item.nama_pengakses}
+                              </td>
+                              <td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
+                                {item.jenis_dokumen}
+                              </td>
+                              <td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
+                                {getISODatetoWIB(item.tanggal)} WIB
+                              </td>
+                              <td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
+                                {(() => {
+                                  switch (item.status_diterima) {
+                                    case 1:
+                                      return (
+                                        <p className='font-bold text-green-500'>
+                                          Diterima
+                                        </p>
+                                      );
+                                    case -1:
+                                      return (
+                                        <p className='font-bold text-red-500'>
+                                          Ditolak
+                                        </p>
+                                      );
+                                    case 0:
+                                      return (
+                                        <div className='flex space-x-2'>
+                                          <button
+                                            onClick={() =>
+                                              verifyAccess(
+                                                Status.DITERIMA,
+                                                item.id_riwayat
+                                              )
+                                            }
+                                            className='inline-flex items-center rounded border border-transparent bg-green-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2'
+                                          >
+                                            Terima
+                                          </button>
+                                          <button
+                                            onClick={() =>
+                                              verifyAccess(
+                                                Status.DITOLAK,
+                                                item.id_riwayat
+                                              )
+                                            }
+                                            className='inline-flex items-center rounded border border-transparent bg-red-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2'
+                                          >
+                                            Tolak
+                                          </button>
+                                        </div>
+                                      );
+                                  }
+                                })()}
+                              </td>
+                            </tr>
+                          ))
+                        )}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
         <Toaster />
